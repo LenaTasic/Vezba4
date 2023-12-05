cd31d3c764af4d25d2439771d76097dc0634e700
LenaTasic <97707992+LenaTasic@users.noreply.github.com>
2023-12-05 11:15:01 -0500

    Create Vezba4.java

---
+++ Vezba4.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Cao");
+        System.out.println("svima");
+        System.out.println("kako");
+        System.out.println("ste");
+        System.out.println("danas?");
+    }
+}
